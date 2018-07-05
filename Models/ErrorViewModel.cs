96b49e9ed990458c22a8d7cf4aa017a9567b7276
Eric Sowell <esowell@onetechnologies.net>
2018-07-05 16:18:37 -0500

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace jenkins_build.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
